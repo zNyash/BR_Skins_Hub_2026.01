d6681c19c82bbc8924ccb9f8772436cc4e1f7b31
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2026-01-24 16:23:38 -0300

    feat: add username refresh endpoint on the server

---
+++ server/api/player/refresh.ts
@@ -0,0 +1,23 @@
+import { auth, v2 } from "osu-api-extended";
+
+export default defineEventHandler(async (event) => {
+  const { osu_id } = getQuery(event);
+  if (!osu_id) {
+    throw new Error("No osu_id provided");
+  }
+
+  await auth.login({
+    type: "v2",
+    client_id: useRuntimeConfig().public.osuClientId,
+    client_secret: useRuntimeConfig().osuClientSecret,
+    scopes: ["public"],
+  });
+
+  const users = await v2.users.lookup({ ids: [Number(osu_id)] });
+  const user = users[0];
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+  return user.username;
+});
