32964a6fd9aa526e1d8cca96cfcdd7468c0c4248
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2026-01-25 13:39:38 -0300

    feat: add player deletion confirmation and improve the UI feedback

--- app/components/PlayerCard.vue
+++ app/components/PlayerCard.vue
@@ -1,12 +1,18 @@
 <template>
   <Transition name="player-card">
-    <div v-if="!isDeleting" class="bg-muted/50 border-muted/50 flex flex-row items-center gap-2 rounded-md border p-2">
+    <div
+      v-if="!hasBeenDeleted"
+      :class="[
+        'bg-muted/50 border-muted/50 flex flex-row items-center gap-2 rounded-md border p-2 transition-opacity',
+        isDeleting ? 'pointer-events-none opacity-50 grayscale' : '',
+      ]"
+    >
       <UAvatar :alt="name" :src="`https://a.ppy.sh/${osu_id}`" />
       <span class="flex flex-col justify-center">
         <p class="font-medium">{{ name }}</p>
         <p class="text-muted -mt-1 text-xs">{{ osu_id }}</p>
       </span>
-      <span class="ml-auto">
+      <span class="ml-auto flex items-center gap-1">
         <UTooltip text="Refresh Username">
           <UButton
             icon="lucide:refresh-ccw"
@@ -16,9 +22,19 @@
             :loading="isRefreshingUsername"
           />
         </UTooltip>
-        <ModalsEditPlayer :player-id="_id" :player-name="name" />
-        <UTooltip text="Delete Player">
-          <UButton icon="lucide:trash-2" @click="handlePlayerDelete" variant="ghost" color="error" />
+
+        <ModalsEditPlayer :player-id="_id" :player-name="name" :osu-id="osu_id" />
+
+        <!-- Lógica de Confirmação de Deleção -->
+        <UTooltip :text="confirmDelete ? 'Click again to confirm' : 'Delete Player'">
+          <UButton
+            :icon="confirmDelete ? 'lucide:alert-triangle' : 'lucide:trash-2'"
+            @click="handleDeleteClick"
+            variant="ghost"
+            :color="confirmDelete ? 'warning' : 'error'"
+            :loading="isDeleting"
+            @mouseleave="confirmDelete = false" 
+          />
         </UTooltip>
       </span>
     </div>
@@ -36,61 +52,88 @@ const props = defineProps<{
 }>();
 
 const toast = useToast();
+
+
+/* --------------------------- */
+/* Deletion Confirmation Logic */
+/* --------------------------- */
+const hasBeenDeleted = ref(false);
+const confirmDelete = ref(false);
+const handleDeleteClick = () => {
+  if (!confirmDelete.value) {
+    confirmDelete.value = true;
+    setTimeout(() => {
+      confirmDelete.value = false;
+    }, 3000);
+  } else {
+    handlePlayerDelete();
+  }
+};
+
+/* --------------------- */
+/* Player Deletion Logic */
+/* --------------------- */
 const isDeleting = ref(false);
-const isRefreshingUsername = ref(false);
 const deletePlayerMutation = useConvexMutation(api.players.deletePlayer);
-
 const handlePlayerDelete = async () => {
-  const mutationState = toast.add({
-    icon: "svg-spinners:ring-resize",
-    title: "Deleting player...",
-    description: `Player ${props.name} is being deleted in the background...`,
-    color: "info",
-    duration: 0,
-  });
-
   try {
     isDeleting.value = true;
     await deletePlayerMutation.mutate({ player_id: props._id });
 
-    toast.update(mutationState.id, {
-      icon: "material-symbols:check-small-rounded",
-      title: "Successfully deleted player",
-      description: `Player ${props.name} has been deleted successfully.`,
+    hasBeenDeleted.value = true;
+
+    toast.add({
+      icon: "lucide:trash-2",
+      title: `Deleted the player ${props.name}`,
       color: "success",
-      duration: 3000,
+      duration: 2000,
     });
   } catch (error) {
     console.error("Error deleting player:", error);
-
-    toast.update(mutationState.id, {
-      icon: "material-symbols:error-outline-rounded",
-      title: "Error deleting player",
-      description: `There was an error deleting player ${props.name}. Please try again.`,
-      color: "error",
-      duration: 3000,
-    });
-  } finally {
     isDeleting.value = false;
+    confirmDelete.value = false;
+
+    toast.add({
+      icon: "lucide:circle-x",
+      title: "Error deleting player",
+      description: (error as Error).message,
+      color: "error",
+    });
   }
 };
 
+/* ----------------------------- */
+/* Player Username Refresh Logic */
+/* ----------------------------- */
+const isRefreshingUsername = ref(false);
 const mutationPlayerUpdate = useConvexMutation(api.players.updatePlayer);
 const handlePlayerNameRefresh = async () => {
   try {
     isRefreshingUsername.value = true;
 
-    const response = await $fetch("/api/player/refresh", {
+    const username = await $fetch("/api/player/refresh", {
       method: "GET",
       params: { osu_id: props.osu_id },
     });
 
     await mutationPlayerUpdate.mutate({
       id: props._id,
-      name: response,
+      name: username,
+    });
+
+    toast.add({
+      title: "Username updated!",
+      color: "success",
+      duration: 1500,
     });
   } catch (error) {
     console.error("Error refreshing player name:", error);
+    toast.add({
+      icon: "lucide:alert-circle",
+      title: "Failed to refresh name",
+      description: "Could not connect to osu! API",
+      color: "error",
+    });
   } finally {
     isRefreshingUsername.value = false;
   }
