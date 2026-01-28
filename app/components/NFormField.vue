61a27f75fc2a7373031f592d3c94d3d3c62f8d96
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2026-01-28 14:51:12 -0300

    feat: add NFormField and NSaveCancel components for form handling

---
+++ app/components/NFormField.vue
@@ -0,0 +1,15 @@
+<template>
+  <span class="flex flex-col items-start">
+    <p class="text-toned text-xs">{{ label }}<span v-if="required" class="text-red-400">*</span></p>
+    <slot />
+  </span>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  label?: string;
+  required?: boolean;
+}>();
+</script>
+
+<style></style>
---
+++ app/components/NSaveCancel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex w-full justify-between gap-2">
+    <span class="my-auto text-left text-xs"> {{ loadingText }} </span>
+    <span class="flex gap-2">
+      <UButton label="Cancel" variant="soft" color="neutral" @click="$emit('cancel')" />
+      <UButton label="Save" @click="$emit('save')" :loading="!!loadingText" />
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  loadingText?: string;
+}>();
+defineEmits<{
+  (e: "cancel"): void;
+  (e: "save"): void;
+}>();
+</script>
+
+<style></style>
