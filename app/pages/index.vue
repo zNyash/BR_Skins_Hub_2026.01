2d89f14d17dc376083d5d3a7eb489f123cbcc1a4
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2026-02-26 08:30:53 -0300

    fix(MainPage/UX): the search bar always render even if the players didnt load yet

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex w-full flex-col items-center">
-    <div v-if="playersList" class="flex w-full max-w-2xl flex-col gap-4">
+    <div class="flex w-full max-w-2xl flex-col gap-4">
       <UInput
         v-model="inputSearch"
         :icon="ICONS.SEARCH"
@@ -8,35 +8,40 @@
         class="w-full max-w-60"
       />
 
-      <div v-if="filteredPlayers?.length" class="grid w-full grid-cols-2 gap-2">
+      <div v-if="filteredPlayers.length" class="grid w-full grid-cols-2 gap-2">
         <MainPlayerCard v-for="player in filteredPlayers" :key="player._id" :player="player" />
       </div>
 
-      <div v-else class="text-muted-foreground flex flex-col items-center justify-center py-10">
+      <div
+        v-else-if="!filteredPlayers.length && !isLoadingPlayers"
+        class="text-muted-foreground flex flex-col items-center justify-center py-10"
+      >
         <UIcon :name="ICONS.FROWN" class="text-muted-foreground/50 mb-2 h-10 w-10" />
         <p>No players found matching "{{ inputSearch }}"</p>
       </div>
-    </div>
-    <div v-else class="grid w-full max-w-2xl grid-cols-2 gap-2">
-      <USkeleton class="h-14 w-full" />
-      <USkeleton class="h-14 w-full" />
-      <USkeleton class="h-14 w-full" />
-      <USkeleton class="h-14 w-full" />
-      <USkeleton class="h-14 w-full" />
-      <USkeleton class="h-14 w-full" />
-      <USkeleton class="h-14 w-full" />
-      <USkeleton class="h-14 w-full" />
+
+      <div v-if="isLoadingPlayers" class="grid w-full max-w-2xl grid-cols-2 gap-2">
+        <USkeleton class="h-14 w-full" />
+        <USkeleton class="h-14 w-full" />
+        <USkeleton class="h-14 w-full" />
+        <USkeleton class="h-14 w-full" />
+        <USkeleton class="h-14 w-full" />
+        <USkeleton class="h-14 w-full" />
+        <USkeleton class="h-14 w-full" />
+        <USkeleton class="h-14 w-full" />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useSorted } from "@vueuse/core";
 import Fuse from "fuse.js";
 import { ICONS } from "~/types/icons";
 import { api } from "~~/convex/_generated/api";
 
 // ------ External Composables ------
-const { data: playersList } = useConvexQuery(api.players.listPlayers);
+const { data: playersList, isPending: isLoadingPlayers } = useConvexQuery(api.players.listPlayers);
 
 // ------ Local State ------
 const inputSearch = ref("");
