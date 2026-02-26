9c2b28092ca8d0cccc10b348445679cbe4856c32
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2026-02-26 18:49:12 -0300

    feat(page/skins): create the structure of the page ready to use.
    - add grid displaying all skins
    - sort all skins by ascending order
    - add search bar to find skins

--- app/pages/skins.vue
+++ app/pages/skins.vue
@@ -1,13 +1,52 @@
 <template>
-  <div>
-    
+  <div class="flex w-full flex-col items-center">
+    <section class="flex w-full max-w-2xl flex-col items-center gap-4">
+      <div class="flex w-full">
+        <UInput
+          v-model="inputSearch"
+          placeholder="Search skins..."
+          :icon="ICONS.SEARCH"
+          class="search-input-default-size"
+        />
+      </div>
+
+      <div class="grid w-full grid-cols-2 gap-2">
+        <LoadingPage :isLoading="isLoadingSkins" />
+
+        <SkinCard v-for="skin in filteredSkins" :key="skin._id" :skin="skin" />
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ICONS } from "~/types/icons";
+import { api } from "~~/convex/_generated/api";
+import { useSorted } from "@vueuse/core";
+import Fuse from "fuse.js";
 
+// ------ External Composables ------
+const { data: skinsList, isPending: isLoadingSkins } = useConvexQuery(api.skins.listSkins);
+
+// ------ Local State ------
+const inputSearch = ref("");
+
+// ------ Computed ------
+const sortedSkins = useSorted(
+  () => skinsList.value || [],
+  (a, b) => a.name.localeCompare(b.name),
+);
+
+const filteredSkins = computed(() => {
+  if (!inputSearch.value) return sortedSkins.value;
+
+  const fuse = new Fuse(sortedSkins.value, {
+    keys: ["name", "author"],
+    threshold: 0.3,
+  });
+
+  return fuse.search(inputSearch.value).map((result) => result.item);
+});
 </script>
 
-<style>
-
-</style>
+<style></style>
